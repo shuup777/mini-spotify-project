53aaf2b833711c3270f3b18e372a106134aa78f7
senoadji1 <spaceimpact124@gmail.com>
2025-11-23 17:25:28 +0700

    isi kode
    belum ada revisi

--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,16 @@
+# users/urls.py
+from django.urls import path
+from . import views
 
+app_name = 'users'
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+    path('profile/update/', views.update_profile_view, name='update_profile'),
+    path('play/<int:song_id>/', views.play_song_view, name='play_song'),
+    path('like/<int:song_id>/', views.like_song_view, name='like_song'),
+    # Tambahkan URL lainnya seperti untuk notifikasi, playlist, dll.
+]
